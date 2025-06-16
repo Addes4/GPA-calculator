a6ce3a75fd65e19314e8eac2852fd5e3a71caff9
Addes4 <167449994+Addes4@users.noreply.github.com>
2025-06-16 12:03:34 +0200

    Update dependencies and fix Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GPA-calculator/',
+  base: process.env.NODE_ENV === 'production' ? '/GPA-calculator/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
